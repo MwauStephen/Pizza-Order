1fbb2551c2c9d7299d7792fa77ed23808fa5765c
Mwau stephen <stephenmwau41@gmail.com>
2022-04-15 12:34:53 +0800

    wrapped the icon inside a span elemeny

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,12 +1,15 @@
 import React from "react";
 import CartIcon from "../Cart/CartIcon";
+import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   return (
-    <button>
-      <CartIcon />
+    <button className={styles.button}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
       <span>Your cart</span>
-      <span>3</span>
+      <span className={styles.badge}>3</span>
     </button>
   );
 };
