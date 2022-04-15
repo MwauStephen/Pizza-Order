0299b2c12ad6c5c2bb8ebc2b5b12d4636fc78bb0
Mwau stephen <stephenmwau41@gmail.com>
2022-04-15 21:54:25 +0800

    added the input attributes

--- src/Components/UI/Input.js
+++ src/Components/UI/Input.js
@@ -1,13 +1,7 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-// const Input = React.forwardRef((ref, props) => {
-//   <div className={styles.input}>
-//     <label htmlFor={props.id}>{props.label}</label>
-//     <input ref={ref} {...props.input} />
-//   </div>
-// )
-// };
+
 
 const Input = React.forwardRef((ref, props) => {
   return (
