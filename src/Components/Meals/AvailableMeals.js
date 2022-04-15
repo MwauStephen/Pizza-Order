0de729c784097e526568b560d525ca13021b54bd
Mwau stephen <stephenmwau41@gmail.com>
2022-04-15 16:20:13 +0800

    changed the contents of the dummy meals array

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -5,40 +5,49 @@ import styles from "./AvailableMeals.module.css";
 const DUMMY_MEALS = [
   {
     id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
+    name: "Pepperoni Pizza",
+    description: "crispy, salty round  pepperoni",
     price: 22.99,
   },
+
   {
     id: "m2",
     name: "Cheese Pizza",
-    description: "A german specialty!",
+    description: "Emmental, Romano and pizza cheeses!",
     price: 16.5,
   },
   {
     id: "m3",
     name: "Veggie Pizza",
-    description: "American, raw, meaty",
+    description: "Finnest mushrooms, eggplant onions",
     price: 12.99,
   },
+
   {
     id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
+    name: " Meat Pizza",
+    description: "Pile of beef and sausage a hearty meal.",
     price: 18.99,
   },
 ];
 
 const AvailableMeals = (props) => {
   const mealsList = DUMMY_MEALS.map((meal) => {
-    <MealItem
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />;
+    return (
+      <MealItem
+        id={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
   });
-  return <ul>{mealsList}</ul>;
+
+  return (
+    <section className={styles.meals}>
+      <ul>{mealsList}</ul>;
+    </section>
+  );
 };
 
 export default AvailableMeals;
