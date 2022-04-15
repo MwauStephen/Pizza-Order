28c63616dfb295fd62570dd338be30465c4a8439
Mwau stephen <stephenmwau41@gmail.com>
2022-04-15 15:47:02 +0800

    added the list structure

--- src/Components/Meals/MealsItem.js
+++ src/Components/Meals/MealsItem.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const MealItem = () => {
-  return;
+const MealItem = (props) => {
+  return (
+    <li>
+      <div>
+        <h3></h3>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </li>
+  );
 };
 
 export default MealItem;
