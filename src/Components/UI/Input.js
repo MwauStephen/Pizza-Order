5a81f24c2b81f0af5fb389037bb8d810d198b667
Mwau stephen <stephenmwau41@gmail.com>
2022-04-15 22:04:31 +0800

    removed refs from the input components

--- src/Components/UI/Input.js
+++ src/Components/UI/Input.js
@@ -1,15 +1,13 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-
-
-const Input = React.forwardRef((ref, props) => {
+const Input = (props) => {
   return (
     <div className={styles.input}>
       <label htmlFor={props.id}>{props.label}</label>
-      <input ref={ref} {...props.input} />
+      <input {...props.input} />
     </div>
   );
-});
+};
 
 export default Input;
