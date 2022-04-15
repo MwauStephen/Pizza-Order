bd55c231ba7b12670f7dbceb8d4865761ca02052
Mwau stephen <stephenmwau41@gmail.com>
2022-04-15 12:19:44 +0800

    imported and added a logo to the header

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,16 +1,21 @@
 import React, { Fragment } from "react";
 import HeaderCartButton from "./HeaderCartButton";
+import mealImg from "../../Assets/pizza5.jpg";
+import logo from "../../Assets/logo.jpg";
 import styles from "./Header.module.css";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={styles.header}>
-        <h1>Pizza app</h1>
+        <h1 className={styles.h1}>
+          <img src={logo} alt="food logo" className={styles.logo} />
+          Pizza app
+        </h1>
         <HeaderCartButton />
       </header>
-      <div>
-        <img src="" alt="Delicous pizza food" />
+      <div className={styles["main-image"]}>
+        <img src={mealImg} alt="Delicous pizza food" />
       </div>
     </Fragment>
   );
