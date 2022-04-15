9495cf2c3daddc0af2cdb88a03c4940059d2049b
Mwau stephen <stephenmwau41@gmail.com>
2022-04-15 10:58:47 +0800

    added the header cart button to the header component

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
 import styles from "./Header.module.css";
 
 const Header = (props) => {
   return (
     <Fragment>
-      <header >
+      <header className={styles.header}>
         <h1>Pizza app</h1>
+        <HeaderCartButton />
       </header>
       <div>
         <img src="" alt="Delicous pizza food" />
