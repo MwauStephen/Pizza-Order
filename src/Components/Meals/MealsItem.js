7eda4cc68afe83b296ac2de137f64f767ee4c1af
Mwau stephen <stephenmwau41@gmail.com>
2022-04-15 22:07:09 +0800

    refactored the div element and added props on the form

--- src/Components/Meals/MealsItem.js
+++ src/Components/Meals/MealsItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MealItemForm from "./MealItemForm";
 
 import styles from "./MealsItem.module.css";
 
@@ -11,7 +12,9 @@ const MealsItem = (props) => {
         <h3>{props.name}</h3>
         <div className={styles.description}>{props.description}</div>
         <div className={styles.price}>{price}</div>
-        <div></div>
+      </div>
+      <div>
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
