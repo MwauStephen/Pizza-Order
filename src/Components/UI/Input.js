d73b9e9a61f922adda196beab74bba7ce1746f06
Mwau stephen <stephenmwau41@gmail.com>
2022-04-15 21:42:31 +0800

    added refs to the input element

--- src/Components/UI/Input.js
+++ src/Components/UI/Input.js
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-const Input = (props) => {
-  <div className={styles.input}>
-    <label htmlFor={props.id}>{props.label}</label>
-    <input />
-  </div>;
-};
+// const Input = React.forwardRef((ref, props) => {
+//   <div className={styles.input}>
+//     <label htmlFor={props.id}>{props.label}</label>
+//     <input ref={ref} {...props.input} />
+//   </div>
+// )
+// };
+
+const Input = React.forwardRef((ref, props) => {
+  return (
+    <div className={styles.input}>
+      <label htmlFor={props.id}>{props.label}</label>
+      <input ref={ref} {...props.input} />
+    </div>
+  );
+});
 
 export default Input;
