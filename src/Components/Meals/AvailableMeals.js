01bab25219951341c9861aa9fc5d5aa240aa0aaa
Mwau stephen <stephenmwau41@gmail.com>
2022-04-15 15:37:27 +0800

    added dummy array for users

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,6 +1,34 @@
 import React from "react";
 import styles from "./AvailableMeals.module.css";
-const AvailableMeals = () => {
+
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Cheese Pizza",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Veggie Pizza",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
+
+const AvailableMeals = (props) => {
   return;
 };
 
