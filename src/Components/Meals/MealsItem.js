d37d370773b1c4f8c907a4dd4e2039cdfa505de8
Mwau stephen <stephenmwau41@gmail.com>
2022-04-15 21:07:04 +0800

    added a price constant

--- src/Components/Meals/MealsItem.js
+++ src/Components/Meals/MealsItem.js
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./MealsItem.module.css";
 
 const MealItem = (props) => {
+  const price = `$ ${props.price.toFixed(2)}`;
+
   return (
     <li className={styles.meal}>
       <div>
         <h3>{props.name}</h3>
         <div className={styles.description}>{props.description}</div>
-        <div className={styles.price}>{props.price}</div>
+        <div className={styles.price}>{price}</div>
         <div></div>
       </div>
     </li>
