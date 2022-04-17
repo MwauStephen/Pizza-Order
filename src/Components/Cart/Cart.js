73ef811b1fd071b2fb5c2f6e6eedad76a72d0416
Mwau stephen <stephenmwau41@gmail.com>
2022-04-17 15:02:26 +0800

    passed the hide cart handler updating function as a prop in the close button

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,7 +2,7 @@ import React from "react";
 import Modal from "../UI/Modal";
 import styles from "./Cart.module.css";
 
-const Cart = () => {
+const Cart = (props) => {
   const cartItems = (
     <ul className={styles["cart-items"]}>
       {[{ id: "c1", name: "sushii", amount: 2, price: 12.99 }].map((item) => {
@@ -18,7 +18,9 @@ const Cart = () => {
         <span>35.99</span>
       </div>
       <div className={styles.actions}>
-        <button className={styles["button--alt"]} onClick>Close</button>
+        <button className={styles["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
         <button className={styles.button}>Order</button>
       </div>
     </Modal>
