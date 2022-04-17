b242935461283235d5dd6b06bcbefcc20cf05026
Mwau stephen <stephenmwau41@gmail.com>
2022-04-17 18:05:15 +0800

    added an error message conditionally

--- src/Components/Meals/MealItem/MealItemForm.js
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -3,6 +3,7 @@ import Input from "../../UI/Input";
 import styles from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
   const amountInputRef = useRef();
 
   const submitHandler = (event) => {
@@ -16,6 +17,7 @@ const MealItemForm = (props) => {
       enteredAmountNumber < 1 ||
       enteredAmountNumber > 5
     ) {
+      setAmountIsValid(false);
       return;
     }
   };
@@ -34,6 +36,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add </button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
   );
 };
