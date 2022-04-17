54a0e9cb1fc9d6b01a9de58c5c62998ad17070e8
Mwau stephen <stephenmwau41@gmail.com>
2022-04-17 14:06:28 +0800

    added a return statement on the map method

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -6,7 +6,7 @@ const Cart = () => {
   const cartItems = (
     <ul className={styles["cart-items"]}>
       {[{ id: "c1", name: "sushii", amount: 2, price: 12.99 }].map((item) => {
-        <li>{item.name}</li>;
+        return <li>{item.name}</li>;
       })}
     </ul>
   );
@@ -24,3 +24,5 @@ const Cart = () => {
     </Modal>
   );
 };
+
+export default Cart;
