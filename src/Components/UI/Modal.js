c8b63ce4c3820c07fbc33da4dab59027687fde4e
Mwau stephen <stephenmwau41@gmail.com>
2022-04-17 13:43:05 +0800

    added two other components inisde the modal component

--- src/Components/UI/Modal.js
+++ src/Components/UI/Modal.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+const BackDrop = () => {
+  return <div>{props.children}</div>;
+};
+
+const ModalOverlay = () => {
+  return <div>{props.children}</div>;
+};
+
 const Modal = () => {
   return;
 };
