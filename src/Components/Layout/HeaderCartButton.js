c9c220046146bcf308f0b4a8afc75ed2706310aa
Mwau stephen <stephenmwau41@gmail.com>
2022-04-17 16:26:50 +0800

    manage state using context to calclate number of items to add to cart

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -4,14 +4,20 @@ import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  // add the items to cart
   const cartCtx = useContext(CartContext);
+
+  // calculates the total items to be added to cart from array items
+  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+    return currNumber + item.amount;
+  }, 0);
   return (
     <button className={styles.button} onClick={props.onClickHandler}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
