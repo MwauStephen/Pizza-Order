6f47b9674e5f9627b80eac7ada9ff3ebc913fc62
Mwau stephen <stephenmwau41@gmail.com>
2022-04-17 14:07:10 +0800

    changed the import path

--- src/Components/UI/Modal.js
+++ src/Components/UI/Modal.js
@@ -1,18 +1,18 @@
 import React, { Fragment } from "react";
-import { ReactDOM } from "react";
+import ReactDOM from "react-dom";
 import styles from "./Modal.module.css";
 
 const BackDrop = () => {
   return <div className={styles.backdrop}></div>;
 };
 
-const ModalOverlay = () => {
+const ModalOverlay = (props) => {
   return <div className={styles.modal}>{props.children}</div>;
 };
 
 const portalElement = document.getElementById("overlays");
 
-const Modal = () => {
+const Modal = (props) => {
   return (
     <Fragment>
       {ReactDOM.createPortal(<BackDrop />, portalElement)}
