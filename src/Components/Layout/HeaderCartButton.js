771b6c840fa85528473a8e0228ff21c557c93c6d
Mwau stephen <stephenmwau41@gmail.com>
2022-04-17 16:23:00 +0800

    stored the cart  context into a varibale

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
+import CartContext from "../../Store/Cart-context";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
   return (
     <button className={styles.button} onClick={props.onClickHandler}>
       <span className={styles.icon}>
