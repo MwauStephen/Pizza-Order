61bf6346edd735db8df623b03afb5ae4bea24784
Mwau stephen <stephenmwau41@gmail.com>
2022-04-17 13:04:31 +0800

    added a cart component and added the structure

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div>
+      cartItems
+      <div>
+        <span>Total Amount</span>
+        <span>35.99</span>
+      </div>
+      <div>
+        <button></button>
+        <button></button>
+      </div>
+    </div>
+  );
+};
