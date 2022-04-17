d374e0a3c4ca40166c1ff239fa1be943683b6f89
Mwau stephen <stephenmwau41@gmail.com>
2022-04-17 14:27:05 +0800

    imported state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 import Cart from "./Components/Cart/Cart";
 
 const App = () => {
+ 
   return (
     <Fragment>
       <Cart />
