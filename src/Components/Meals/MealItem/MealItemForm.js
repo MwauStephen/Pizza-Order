502e6b554e31f36b0e094997df1105410296844a
Mwau stephen <stephenmwau41@gmail.com>
2022-04-17 17:22:06 +0800

    imported and used ref on the input component ,but it wont work unless refs are forwarded

--- src/Components/Meals/MealItem/MealItemForm.js
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import Input from "../../UI/Input";
 import styles from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+
   const submitHandler = (event) => {
     event.preventDefault();
   };
   return (
     <form className={styles.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount" + props.id,
