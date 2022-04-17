95dcbd4b72a2015d1b4bf6cfb3cbe528336715bd
Mwau stephen <stephenmwau41@gmail.com>
2022-04-17 13:13:18 +0800

    added some dummy array in the cart component

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,9 +1,17 @@
 import React from "react";
+import styles from "./Cart.module.css";
 
 const Cart = () => {
+  const cartItems = (
+    <ul>
+      {[{ id: "c1", name: "sushii", amount: 2, price: 12.99 }].map((item) => {
+        <li>{item.name}</li>;
+      })}
+    </ul>
+  );
   return (
     <div>
-      cartItems
+      {cartItems}
       <div>
         <span>Total Amount</span>
         <span>35.99</span>
